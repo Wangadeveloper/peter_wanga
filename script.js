266ef79bfb2e16edcbded1c0ca4dfad470f5815d
Peter Wanga <139241991+Wangadeveloper@users.noreply.github.com>
2024-11-29 03:54:52 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleMenu(){
+    const menuContent=document.getElementById('menu-content');
+    if(menuContent.style.display=== 'block'){
+        menuContent.style.display= 'none';
+
+    }
+    else{
+        menuContent.style.display='block'
+
+    }
+}
